9a3982e32c3a61a13635c1d2421300dcb1131e0d
kxie724 <kunxie0714@163.com>
2024-08-14 21:34:25 +0800

    update

--- DGCluster/main.py
+++ DGCluster/main.py
@@ -24,12 +24,19 @@ import utils
 import os
 
 
+import pdb, time
+from datetime import datetime
+
+start_time = time.time()
+print('start time:', datetime.now())
+
+
 def parse_args():
     args = argparse.ArgumentParser(description='DGCluster arguments.')
     args.add_argument('--dataset', type=str, default='cora')
     args.add_argument('--lam', type=float, default=0.2)
     args.add_argument('--alp', type=float, default=0.0)
-    args.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
+    args.add_argument('--device', type=str, default='cuda:0', choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
     args.add_argument('--epochs', type=int, default=300)
     args.add_argument('--base_model', type=str, default='gcn', choices=['gcn', 'gat', 'gin', 'sage'])
     args.add_argument('--seed', type=int, default=0)
@@ -253,90 +260,123 @@ if __name__ == '__main__':
 
     in_dim = data.x.shape[1]
     out_dim = 64
-    if args.encoder == 'GCN':
-        model = GNN(in_dim, out_dim, base_model=base_model).to(device)
-    elif args.encoder == 'low_pass':
+
+    runs = 5
+    total_res_B = []
+    total_res_K = []
+    for fold in range(5):
+        if args.encoder == 'GCN':
+            model = GNN(in_dim, out_dim, base_model=base_model).to(device)
+        elif args.encoder == 'low_pass':
+            import sys, pdb
+            sys.path.append('..')
+            from model import low_pass_model
+            from dgc.utils import load_graph_data, normalize_adj, construct_filter, normalize_adj_torch
+            A = torch.FloatTensor(sparse_adj.toarray()).to(device)
+            A = normalize_adj_torch(A, self_loop=True, symmetry=True)
+            low_pass_filter = construct_filter(A, l=3, alpha=0.5) 
+            args.first_transformation = 'linear'
+            model = low_pass_model(args, low_pass_filter, in_dim, out_dim).to(device)
+        else:
+            raise NotImplementedError
+
+        optimizer_name = "Adam"
+        lr = 1e-3
+        optimizer = getattr(torch.optim, optimizer_name)(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.001, amsgrad=True)
+
+        print('#'*60)
+        print('training...')
+        print('#'*60)
+        train(model, optimizer, data, epochs, lam, alp)
+
+        print('#'*60)
+        print('testing...')
+        print('#'*60)
+        test_data = data.clone()
+        print(test_data)
+
+        model.eval()
+        if args.encoder == 'GCN':
+            x = model(test_data)
+        elif args.encoder == 'low_pass':
+            out = model(data.x)
+            x = F.normalize(out)
+
+
+        print('#'*60)
+        print('evaluating...')
+        print('#'*60)
+        clusters = Birch(n_clusters=None, threshold=0.5).fit_predict(x.detach().cpu().numpy(), y=None)
+        FQ = utils.compute_fast_modularity(clusters, num_nodes, num_edges, torch_sparse_adj, degree, device)
+        # print('No of clusters: ', max(clusters) + 1)
+        # print('Modularity:', FQ)
+
+        # NMI = utils.compute_nmi(clusters, data.y.squeeze().cpu().numpy())
+        # print('NMI:', NMI)
+
+        # conductance = utils.compute_conductance(clusters, Graph)
+        # avg_conductance = sum(conductance) / len(conductance)
+        # print('conductance', avg_conductance * 100)
+
+        # f1_score = utils.sample_f1_score(test_data, clusters, num_nodes)
+        # print('Sample_F1_score:', f1_score)
+
+        # results = {
+        #     'num_clusters': np.unique(clusters).shape[0],
+        #     'modularity': FQ,
+        #     'nmi': NMI,
+        #     'conductance': avg_conductance,
+        #     'sample_f1_score': f1_score
+        # }
+
         import sys, pdb
-        sys.path.append('..')
-        from model import low_pass_model
-        from dgc.utils import load_graph_data, normalize_adj, construct_filter, normalize_adj_torch
-        A = torch.FloatTensor(sparse_adj.toarray()).to(device)
-        A = normalize_adj_torch(A, self_loop=True, symmetry=True)
-        low_pass_filter = construct_filter(A, l=3, alpha=0.5) 
-        args.first_transformation = 'linear'
-        model = low_pass_model(args, low_pass_filter, in_dim, out_dim).to(device)
-    else:
-        raise NotImplementedError
-
-    optimizer_name = "Adam"
-    lr = 1e-3
-    optimizer = getattr(torch.optim, optimizer_name)(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.001, amsgrad=True)
-
-    print('#'*60)
-    print('training...')
-    print('#'*60)
-    train(model, optimizer, data, epochs, lam, alp)
-
-    print('#'*60)
-    print('testing...')
-    print('#'*60)
-    test_data = data.clone()
-    print(test_data)
-
-    model.eval()
-    if args.encoder == 'GCN':
-        out = model(data)
-    elif args.encoder == 'low_pass':
-        out = model(data.x)
-        out = F.normalize(out)
+        sys.path.append('../dgc')
+        from clustering import k_means
+        from eval import print_eval, modularity
+        from scipy.sparse import csr_matrix, coo_matrix
+        adjacency = coo_matrix((np.ones((data.edge_index.shape[1])), 
+                            (data.edge_index[0].cpu().numpy(), data.edge_index[1].cpu().numpy()))).toarray()
+        print('our eval func: ', end='')
+        res_Birch = print_eval(clusters, data.y.cpu().numpy(), adjacency)
+        total_res_B.append(res_Birch)
+        
+        print('kmeans: ', end='')
+        cluster_ids,_ = k_means(x.detach(), len(np.unique(data.y.cpu().numpy())), device='cpu', distance='cosine')
+        res = print_eval(cluster_ids, data.y.cpu().numpy(), adjacency)
+        total_res_K.append(res)
 
 
-    print('#'*60)
-    print('evaluating...')
-    print('#'*60)
-    clusters = Birch(n_clusters=None, threshold=0.5).fit_predict(x.detach().cpu().numpy(), y=None)
-    FQ = utils.compute_fast_modularity(clusters, num_nodes, num_edges, torch_sparse_adj, degree, device)
-    print('No of clusters: ', max(clusters) + 1)
-    print('Modularity:', FQ)
+print('end time:', datetime.now())
+total_time = time.time() - start_time
+print('total time:', total_time)
 
-    NMI = utils.compute_nmi(clusters, data.y.squeeze().cpu().numpy())
-    print('NMI:', NMI)
+log_file = 'res_log.txt'
+log_fold_file = 'res_fold_log.txt'
+log_file_B = 'res_Birch_log.txt'
+log_fold_file_B = 'res_Birch_fold_log.txt'
+print(f"save res to {log_file} and {log_fold_file}, Birch res to {log_file_B} and {log_fold_file_B}")
 
-    conductance = utils.compute_conductance(clusters, Graph)
-    avg_conductance = sum(conductance) / len(conductance)
-    print('conductance', avg_conductance * 100)
+total_res = np.array(total_res_K)
 
-    f1_score = utils.sample_f1_score(test_data, clusters, num_nodes)
-    print('Sample_F1_score:', f1_score)
-
-    results = {
-        'num_clusters': np.unique(clusters).shape[0],
-        'modularity': FQ,
-        'nmi': NMI,
-        'conductance': avg_conductance,
-        'sample_f1_score': f1_score
-    }
-
-    import sys, pdb
-    sys.path.append('../dgc')
-    from clustering import k_means
-    from eval import print_eval, modularity
-    from scipy.sparse import csr_matrix, coo_matrix
-    adjacency = coo_matrix((np.ones((data.edge_index.shape[1])), 
-                           (data.edge_index[0].cpu().numpy(), data.edge_index[1].cpu().numpy()))).toarray()
-    print('our eval func: ', end='')
-    print_eval(clusters, data.y.cpu().numpy(), adjacency)
+with open(log_file, 'a+') as f:
+    # if the file is empty, write the header
+    if os.path.getsize(log_file) == 0:
+        f.write('dataset, acc_mean, acc_std, nmi_mean, nmi_std, ari_mean, ari_std, f1_mean, f1_std, mod_mean, mod_std, con_mean, con_std, time\n')
+    f.write('%s, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n'%(args.dataset, 
+        total_res[:, 0].mean()*100, total_res[:, 0].std()*100, 
+        total_res[:, 1].mean()*100, total_res[:, 1].std()*100, 
+        total_res[:, 2].mean()*100, total_res[:, 2].std()*100, 
+        total_res[:, 3].mean()*100, total_res[:, 3].std()*100, 
+        total_res[:, 4].mean()*100, total_res[:, 4].std()*100, 
+        total_res[:, 5].mean()*100, total_res[:, 5].std()*100, total_time))
     
-    print('kmeans: ', end='')
-    cluster_ids,_ = k_means(x.detach(), len(np.unique(data.y.cpu().numpy())), device='gpu', distance='cosine')
-    print_eval(cluster_ids, data.y.cpu().numpy(), adjacency)
-    
-    
-    
+with open(log_fold_file, 'a+') as f:
+    # if the file is empty, write the header
+    if os.path.getsize(log_fold_file) == 0:
+        f.write('norm, layers, alpha, S_alpha, ssg0, ssg1, ssg2, ssg3, ')
+        f.write('dataset, fold, acc_mean, nmi_mean, ari_mean, f1_mean, mod_mean, con_mean, time\n')
+    for i in range(total_res.shape[0]):
+        f.write(', '.join([str(x) for x in [args.norm, args.top_layers, args.top_alpha, args.attr_alpha, args.loss_lambda_SSG0, args.loss_lambda_SSG1, args.loss_lambda_SSG2, args.loss_lambda_SSG3]])+', ')
+        f.write('%s, %d, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n'%(args.dataset, i, 
+            total_res[i, 0]*100, total_res[i, 1]*100, total_res[i, 2]*100, total_res[i, 3]*100, total_res[i, 4]*100, total_res[i, 5]*100, total_time))
 
-    if not os.path.exists('results'):
-        os.makedirs('results')
-    if alp == 0.0:
-        torch.save(results, f'results/results_{dataset_name}_{lam}_{epochs}_{base_model}_{seed}.pt')
-    else:
-        torch.save(results, f'results/results_{dataset_name}_{lam}_{alp}_{epochs}_{base_model}_{seed}.pt')
